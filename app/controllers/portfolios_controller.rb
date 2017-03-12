812b66d995e7e8da1c1924065986002acbf51228
gpdno <gdeninno@suncoastde.com>
2017-03-12 13:06:38 -0400

    added persistance to protfolio drag and drop

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,19 +1,22 @@
 class PortfoliosController < ApplicationController
-  before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
+  before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
   layout 'portfolio'
   access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
-
+  
   def index
-    @page_title = "My portfolio Items"
     @portfolio_items = Portfolio.by_position
+  end
+
+  def sort
+    params[:order].each do |key, value|
+      Portfolio.find(value[:id]).update(position: value[:position])
+    end
+
+    render nothing: true
   end
 
   def angular
     @angular_portfolio_items = Portfolio.angular
-  end
-
-  def show
-    @page_title = @portfolio_item.title
   end
 
   def new
@@ -26,7 +29,7 @@ class PortfoliosController < ApplicationController
 
     respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
       else
         format.html { render :new }
       end
@@ -39,31 +42,37 @@ class PortfoliosController < ApplicationController
   def update
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
-        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
       else
         format.html { render :edit }
       end
     end
   end
 
+  def show
+  end
+
   def destroy
+    # Destroy/delete the record
     @portfolio_item.destroy
+
+    # Redirect
     respond_to do |format|
-    format.html { redirect_to portfolios_url, notice: 'Item was successfully destroyed.' }
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
   end
 
   private
-
-  def set_portfolio
-    @portfolio_item = Portfolio.find(params[:id])
-  end
 
   def portfolio_params
     params.require(:portfolio).permit(:title,
                                       :subtitle,
                                       :body,
                                       technologies_attributes: [:name]
-                                      )
+                                     )
+  end
+
+  def set_portfolio_item
+    @portfolio_item = Portfolio.find(params[:id])
   end
 end
