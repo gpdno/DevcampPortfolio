24a01ec0de7050173dbdc6ce05299bb9610444e8
gpdno <gpdno@mac.com>
2017-03-31 13:13:51 -0400

    changed pages background to be dynamic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,16 +2,20 @@ class PagesController < ApplicationController
   def home
     @posts = Blog.all
     @skills = Skill.all
+    @page_bg = "background_home"
   end
 
   def about
     @skills = Skill.all
+    @page_bg = "background_about"
   end
 
   def contact
+    @page_bg = "background_contact"
   end
 
   def tech_news
     @tweets = SocialTools.twitter_search
+    @page_bg = "background_tech_news"
   end
 end
